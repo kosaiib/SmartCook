2d8fa18b4ecd3f6974c2c008357524744ee5497c
sebastian447 <210031@studierende.htl-donaustadt.at>
2025-03-18 10:49:02 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SmartCook',
+  webDir: 'www'
+};
+
+export default config;
