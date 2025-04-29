f6470cc5bce96930a85c0255b84e68b649a449c8
sebastian447 <210031@studierende.htl-donaustadt.at>
2025-04-29 11:39:03 +0200

    Rezepte anhand von Zutaten auflisten

---
+++ src/app/services/meal.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {forkJoin, map, Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MealService {
+
+  private apiUrl = 'https://www.themealdb.com/api/json/v1/1';
+
+  constructor(private http: HttpClient) {}
+
+  getAllIngredients(): Observable<string[]> {
+    return this.http
+      .get<any>(`${this.apiUrl}/list.php?i=list`)
+      .pipe(map(res => res.meals.map((m: any) => m.strIngredient)));
+  }
+
+  searchByIngredient(ingredient: string): Observable<any[]> {
+    return this.http
+      .get<any>(`${this.apiUrl}/filter.php?i=${ingredient}`)
+      .pipe(map(res => res.meals || []));
+  }
+
+  getMealDetails(id: string): Observable<any> {
+    return this.http
+      .get<any>(`${this.apiUrl}/lookup.php?i=${id}`)
+      .pipe(map(res => res.meals[0]));
+  }
+
+  searchByMultipleIngredients(ingredients: string[]): Observable<any[]> {
+    const requests = ingredients.map(i => this.searchByIngredient(i));
+    return forkJoin(requests).pipe(
+      map(results => {
+        const allMeals = results.reduce((acc, val) => acc.concat(val), []);
+
+        const uniqueMeals = new Map();
+        for (let meal of allMeals) {
+          uniqueMeals.set(meal.idMeal, meal);
+        }
+        return Array.from(uniqueMeals.values());
+      })
+    );
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,19 +1,55 @@
-import { Component } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import {ApiService} from "../services/api.service";
 import {TestComponent} from "../test/test.component";
+import {MealService} from "../services/meal.service";
+import {FormsModule} from "@angular/forms";
+import {IonicModule} from "@ionic/angular";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, TestComponent],
+  imports: [ ExploreContainerComponent, TestComponent,  FormsModule,CommonModule, IonicModule],
 })
 export class Tab1Page {
-  constructor() {}
 
+  zutatenListe: string[] = [];
+  ausgewaehlteZutaten: string[] = [];
+  rezepte: any[] = [];
+  neueZutat: string = '';
+
+  constructor(private mealService: MealService) {}
+
+  ngOnInit() {
+    this.mealService.getAllIngredients().subscribe(data => {
+      this.zutatenListe = data;
+    });
+  }
+
+  zutatHinzufuegen() {
+    if (
+      this.neueZutat.trim() &&
+      !this.ausgewaehlteZutaten.includes(this.neueZutat)
+    ) {
+      this.ausgewaehlteZutaten.push(this.neueZutat);
+      this.neueZutat = '';
+    }
+  }
+
+  zutatEntfernen(index: number) {
+    this.ausgewaehlteZutaten.splice(index, 1);
+  }
+
+  rezepteSuchen() {
+    this.mealService
+      .searchByMultipleIngredients(this.ausgewaehlteZutaten)
+      .subscribe(data => {
+        this.rezepte = data;
+      });
+  }
 
 }
 
